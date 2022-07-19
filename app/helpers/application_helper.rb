8cdf5a40bd6dcf1d5b6552fc540d939c714f697b
EC2 Default User <ec2-user@ip-172-31-25-181.ap-northeast-1.compute.internal>
2022-07-19 13:35:28 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+#ページごとにタイトルを返す
+  def full_title(page_name = "")#メソッドと引数の定義
+    base_title = "AttendanceApp"#基本となるアプリケーション名を変数に代入
+    if page_name.empty?#引数を受け取っているか判定
+      base_title#引数page_nameが空文字の場合はAttendanceApp(base_title)を返す
+    else#引数page_nameが空文字でない場合は
+      page_name + " | " + base_title#引数のタイトル名＋｜AttendanceApp(base_title)を返す
+    end
+  end
 end
