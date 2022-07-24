52d48820befe37d7685ed6dc85594d8863680dc6
EC2 Default User <ec2-user@ip-172-31-25-181.ap-northeast-1.compute.internal>
2022-07-24 17:04:41 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
    
     if @user.save
+     log_in @user # 保存成功後、ログインします。
      redirect_to @user
      flash[:success] = '新規作成に成功しました。'
+     redirect_to @user
     else
      render :new
     end
