738ea1b1caac738597835b93cd11ea903730e263
EC2 Default User <ec2-user@ip-172-31-25-181.ap-northeast-1.compute.internal>
2022-07-31 11:31:18 +0000

    Finish edit-attendances

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,11 +3,34 @@ class Attendance < ApplicationRecord
 
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
-  
-   # 出勤時間が存在しない場合、退勤時間は無効
+
+  # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
+  # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
+  validate :started_at_than_finished_at_fast_if_invalid
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+  end
+
+  def started_at_than_finished_at_fast_if_invalid
+    if started_at.present? && finished_at.present?
+      errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
+    end
+  end
+  
+  def update_one_month
+    ActiveRecord::Base.transaction do # トランザクションを開始します。
+      attendances_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+  end
+  flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+  redirect_to user_url(date: params[:date])
+  
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
 end
